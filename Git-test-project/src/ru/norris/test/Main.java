3cbad50d5e2c2fffba24d1abcf80f0251069b4b1
User <User@192.168.1.67>
2017-03-02 22:31:07 +0500

    read

--- Git-test-project/src/ru/norris/test/Main.java
+++ Git-test-project/src/ru/norris/test/Main.java
@@ -3,8 +3,8 @@ package ru.norris.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub!!!");
-
+		System.out.println("Hello from Git Hub!!!!");
+		
 	}
 
 }
